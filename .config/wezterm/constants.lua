081c462f4656c0f531df0f0acf51d37445d5ac95
Alberto Alvarez <alberto.alvarez.96@proton.me>
2025-05-10 12:54:29 -0600

    Added image bg for wezterm and some extra configs
    Signed-off-by: Alberto Alvarez <alberto.alvarez.96@proton.me>

---
+++ .config/wezterm/constants.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.bg_image = os.getenv("HOME") .. "/dotfiles/.config/wezterm/assets/tree.jpg"
+
+return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,28 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local constants = require 'constants'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+-- Color scheme:
 config.color_scheme = 'Catppuccin Mocha'
+
+-- Font size:
+config.font_size = 14
+config.line_height = 1.1
+config.font = wezterm.font("Hack Nerd Font")
+
+-- Appearance
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_image = constants.bg_image
+
+-- Miscellaneous config for Mac
+config.max_fps = 120
+config.prefer_egl = true
 
 -- and finally, return the configuration to wezterm
 return config
